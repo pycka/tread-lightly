7621bc8999873d079bec45ab5e69f4270e959053
Radoslaw Pycka <rpycka+bitbucket.org@gmail.com>
2013-01-26 19:17:04 +0100

    Client side basics

---
+++ public/src/js/display.js
@@ -0,0 +1,16 @@
+define(function () {
+
+  var display = {
+    canvas: null,
+
+    connect: function (canvasElement) {
+      this.canvas = canvasElement;
+    },
+
+    draw: function (world) {
+
+    }
+  };
+
+  return display;
+});
---
+++ public/src/js/input.js
@@ -0,0 +1,23 @@
+define(function () {
+  var KEY_UP      = 1;
+  var KEY_DOWN    = 2;
+  var KEY_LEFT    = 4;
+  var KEY_RIGHT   = 8;
+  var MOUSE_CLICK = 16;
+
+  var input = {
+    // mx, my, button map
+    state: [0, 0, 0],
+
+    connect: function () {
+
+    },
+
+    update: function () {
+
+    },
+
+  };
+
+  return input;
+});
